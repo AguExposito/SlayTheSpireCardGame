ddfc7ec1c5c8abc659546b398756f74ee82ab9bc
AgustinExposito <agustin.exposito@interbrainco.com>
2023-09-24 23:09:33 -0300

    Made Random positioning in y axis (only paiir numbers)

--- Assets/Code/Scripts/MapGenerator.cs
+++ Assets/Code/Scripts/MapGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using GD.MinMaxSlider;
 using Unity.VisualScripting;
 using static UnityEngine.Rendering.HableCurve;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class MapGenerator : MonoBehaviour
 
     [SerializeField] GameObject room;
     [SerializeField] Sprite[] roomsIcon = new Sprite[4];
-    [SerializeField] Vector2[] parVector2;
+    //[SerializeField] Vector2[] parVector2;
     [SerializeField] Vector2[] imparVector2;
 
     [SerializeField] GameObject initialRoom;
@@ -34,8 +35,9 @@ public class MapGenerator : MonoBehaviour
 
     private void GenerateNodes()
     {
-        float initRoomWidthOffsetX = Mathf.Abs(initialRoom.transform.localScale.x/2);
+        float initRoomWidthOffsetY = Mathf.Abs(initialRoom.transform.localScale.y/2);
         float finalRoomWidthOffsetX = Mathf.Abs(finalRoom.transform.localScale.x/2);
+
         float totalDistanceIF = Mathf.Abs((finalRoom.transform.position.x) - (initialRoom.transform.position.x));
         Debug.Log("Dist: " + totalDistanceIF);
 
@@ -47,29 +49,80 @@ public class MapGenerator : MonoBehaviour
 
         Vector2 rectPos = gameObject.GetComponent<RectTransform>().position;
 
+        
+
         for (int k = 0; k< segments; k++) {
 
             initialBranch = Random.Range(initialBranchRooms.x, initialBranchRooms.y+1);
             counter += segmentDist;
-            Debug.Log("Segm: " + counter);
 
-            for (int i = 0; i < initialBranch; i++) {
+            for (int i = 1; i < initialBranch+1; i++) {
 
                 //float roomOffsetX = room.GetComponent<Renderer>().bounds.size.x / 2f; // Adjust for room width
-                float mapOffsetX = (totalDistanceIF) / 2;             
+                float mapOffsetX = (totalDistanceIF) / 2;
+                float roomHeightHalved = (gameObject.GetComponent<Renderer>().bounds.size.y / 2f) - initRoomWidthOffsetY;
+                float roomHeight = (gameObject.GetComponent<Renderer>().bounds.size.y/2) - initRoomWidthOffsetY;
+                float yOffset = initRoomWidthOffsetY * 2;//*2 porque se puede dar que se toquen los nodos
+
+                
 
                 if (initialBranch % 2 == 0)
                 {
-                    Vector2 spawnPositionPar = rectPos + new Vector2(counter - mapOffsetX /* -roomOffsetX */, parVector2[i].y);
+                    float randomPosY=0;
+
+                    if (initialBranch == 2 ) {
+                        
+                        switch (i) { 
+                        case 1: { randomPosY = Random.Range(yOffset, roomHeight); } break;
+                        case 2: { randomPosY = Random.Range(-yOffset, -roomHeight); } break;
+                        }
+
+                    }
+                    else {
+                        switch (i)
+                        {
+                            case 1: { randomPosY = Random.Range(yOffset, roomHeight / 2); } break;
+                            case 2: { randomPosY = Random.Range(-yOffset, -roomHeight / 2); } break;
+                            case 3: { randomPosY = Random.Range(yOffset + (roomHeight / 2), roomHeight); } break;
+                            case 4: { randomPosY = Random.Range(-yOffset - (roomHeight / 2), -roomHeight); } break;
+                        }
+                    }
+
+                    Debug.Log("roomH: " + roomHeight + "yOff: " + yOffset + "RPosY: "+randomPosY);
+
+                    Vector2 spawnPositionPar = rectPos + new Vector2(counter - mapOffsetX /* -roomOffsetX */, randomPosY);
                     Debug.Log("Par");
                     Instantiate(room, spawnPositionPar, Quaternion.identity, gameObject.transform.GetChild(0));
                 }
                 else {
-                    Vector2 spawnPositionImpar = rectPos + new Vector2(counter - mapOffsetX /* -roomOffsetX */, imparVector2[i].y);
-                    Debug.Log("Impar");
-                    Instantiate(room, spawnPositionImpar, Quaternion.identity, gameObject.transform.GetChild(0));
+                    //Vector2 spawnPositionImpar = rectPos + new Vector2(counter - mapOffsetX /* -roomOffsetX */, imparVector2[i].y);
+                    //Debug.Log("Impar");
+                    //Instantiate(room, spawnPositionImpar, Quaternion.identity, gameObject.transform.GetChild(0));
                 }
             }
         }
-    } 
+    }
+
+    private void OnDrawGizmos()    {
+
+        float initRoomWidthOffsetX = Mathf.Abs(initialRoom.transform.localScale.x / 2);
+        float roomHeightHalved = (gameObject.GetComponent<Renderer>().bounds.size.y / 2f) - initRoomWidthOffsetX;
+        float roomHeight = (gameObject.GetComponent<Renderer>().bounds.size.y) - initRoomWidthOffsetX;
+        float yOffset = initRoomWidthOffsetX * 2;//*2 porque se puede dar que se toquen los nodos
+
+
+        Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, yOffset),Color.green);
+        Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, -yOffset), Color.green);
+        Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, roomHeight / 2), Color.green);
+        Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, -roomHeight / 2), Color.green);
+        Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, yOffset + (roomHeight / 2)), Color.red);
+        Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, -yOffset - (roomHeight / 2)), Color.red);
+        Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, roomHeight), Color.red);
+        Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, -(roomHeight)), Color.red);
+
+        //Debug.DrawLine(new Vector2(-1000, roomHeightHalved / 2), new Vector2(1000, (roomHeightHalved / 2)));
+        //Debug.DrawLine(new Vector2(-1000,-roomHeightHalved / 2), new Vector2(1000, (-roomHeightHalved / 2)));
+        //Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, -roomHeight));
+        //Debug.DrawLine(new Vector2(-1000,0), new Vector2(1000, roomHeight));
+    }
 }
